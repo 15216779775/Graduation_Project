2fa3394904ce2368981e866cb08da800a02ebc85
15216779775 <1060232779@qq.com>
2018-03-29 12:05:05 +0800

    task6
    课程进行到任务6

---
+++ Graduation_Project/Assets/Scripts/AvatarSys.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarSys : MonoBehaviour {
+    private GameObject girlSource;//ziyuan 
+    private GameObject girlTarget;//huanzhuangmubiao
+    private Transform girlSourceTransform;
+    private Dictionary<string, Dictionary<string, SkinnedMeshRenderer>>
+        girlData = new Dictionary<string, Dictionary<string, SkinnedMeshRenderer>>();
+    //字典存储信息
+    Transform[] girlHips;
+
+	// Use this for initialization
+	void Start () {
+        InstantiateSources();
+        InstantiateTarget();
+        girlHips = girlTarget.GetComponentsInChildren<Transform>();
+	}
+
+    void InstantiateSources()
+    {
+        girlSource = Instantiate(Resources.Load("femaleModel")) as GameObject;//加载资源
+        girlSourceTransform = girlSource.transform;
+        girlSource.SetActive(false);
+    }
+	void InstantiateTarget()
+    {
+        girlTarget = Instantiate(Resources.Load("femaleTarget"))as GameObject;
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
